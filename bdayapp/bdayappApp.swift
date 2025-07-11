529e8ea4d64d09265e7963dbeb90a0edd8c281d2
Navya <navyazhni@gmail.com>
2025-07-11 07:11:18 -0700

    Initial Commit

---
+++ bdayapp/bdayappApp.swift
@@ -0,0 +1,17 @@
+//
+//  bdayappApp.swift
+//  bdayapp
+//
+//  Created by Scholar on 7/11/25.
+//
+
+import SwiftUI
+
+@main
+struct bdayappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ bdayappTests/bdayappTests.swift
@@ -0,0 +1,17 @@
+//
+//  bdayappTests.swift
+//  bdayappTests
+//
+//  Created by Scholar on 7/11/25.
+//
+
+import Testing
+@testable import bdayapp
+
+struct bdayappTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
