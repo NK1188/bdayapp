b67741673d030c5cabd7bc6fe4e85136ab106756
Navya <navyazhni@gmail.com>
2025-07-11 07:37:29 -0700

    this is the first commit

---
+++ bdayapp/Friend.swift
@@ -0,0 +1,17 @@
+//
+//  Friend.swift
+//  bdayapp
+//
+//  Created by Scholar on 7/11/25.
+//
+
+import Foundation
+
+class Friend {
+    var name : String
+    var birthday : Date
+    init (name: String, birthday : Date){
+        frnd.name = name
+        frnd.birthday= birthday
+    }
+}
